5a622749844d9b6bd2e51ccccc3af8f179be225d
robond <you@example.com>
2019-09-08 06:58:33 -0700

    1st try submit

---
+++ src/scripts/add_markers.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+catkin_dir=~/robo-nd-ws/
+catkin_src_dir=$catkin_dir/src
+
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$catkin_src_dir/worlds/mynewworld.world" &
+sleep 5
+
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$catkin_src_dir/worlds/office_map.yaml" &
+sleep 5
+
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+
+xterm -e " source $catkin_dir/devel/setup.bash; rosrun add_markers add_markers_node"
+
--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:= ~/robo-nd-ws/robond5/worlds/mynewworld.world " &
+catkin_dir=~/robo-nd-ws/
+catkin_src_dir=$catkin_dir/src
+
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$catkin_src_dir/worlds/mynewworld.world " &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+
+xterm -e " rosrun gmapping slam_gmapping" &
 sleep 5
+
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
+
 xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch " &
